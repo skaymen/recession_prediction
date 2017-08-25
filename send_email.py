17f4033a52c86388aa46569bf6853107b375f81c
Stas <stanley.kaymen@gmail.com>
2017-08-24 19:47:26 -0500

    send a dummy email

--- send_email.py
+++ send_email.py
@@ -3,5 +3,9 @@ import config as c
 import smtplib
 
 server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
 
-server.login()
+server.login(c.GMAIL_USERNAME, c.GMAIL_PASSWORD)
+
+msg = 'fuck you'
+server.sendmail(c.GMAIL_USERNAME, 'stanley.kaymen@gmail.com', msg)
